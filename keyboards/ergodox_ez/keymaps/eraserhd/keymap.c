323fd5020b2ad65a44b57097547c3d1c9d5fba58
Jason Felice <jason.m.felice@gmail.com>
2018-05-07 16:27:00 -0400

    Set up L/R Alt

--- keyboards/ergodox_ez/keymaps/eraserhd/keymap.c
+++ keyboards/ergodox_ez/keymaps/eraserhd/keymap.c
@@ -25,11 +25,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | SYMB   |   Q  |   W  |   E  |   R  |   T  | Meh  |           | Meh  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | CAPSL  |   A  |   S  |D/MNAV|F/KNAV|   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / SYMB|
+ * | CAPSL  |   A  |   S  |D/MNAV|F/KNAV|   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |' / SYMB|
  * |--------+------+------+------+------+------| Hyper|           |Hyper |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | `    |  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  |Leader|
+ *   | `    |  '"  |AltShf| Left | LAlt |                                       | RAlt | Down |   [  |   ]  |Leader|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |  Esc |
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(SYMB),    KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   MEH_T(KC_NO),
         KC_CAPSLOCK, KC_A,         KC_S,   LT(MNAV,KC_D),   LT(KNAV,KC_F),   KC_G,
         KC_LSFT,     CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_GRV,      KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
+        KC_GRV,      KC_QUOT,      LALT(KC_LSFT),  KC_LEFT, KC_LALT,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               MO(MNAV),
                                         KC_ENT,LGUI_T(KC_TAB),MO(KNAV),
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              MEH_T(KC_NO),KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,           KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN, 	 LT(SYMB,KC_QUOT),
              ALL_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH), KC_RSFT,
-                                  KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,        KC_LEAD,
+                                  KC_RALT, KC_DOWN,KC_LBRC,KC_RBRC,        KC_LEAD,
              KC_LALT,        KC_ESC,
              KC_PGUP,
              KC_PGDN, LGUI_T(KC_BSPC), KC_SPC
