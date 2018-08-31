1d4e8079c81649f0e3f6a02613b11a4d77bf2454
Jason Felice <jason.m.felice@gmail.com>
2018-08-31 10:08:31 -0400

    Real alt keys

--- keyboards/ergodox_ez/keymaps/eraserhd/keymap.c
+++ keyboards/ergodox_ez/keymaps/eraserhd/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | SYMB   |   Q  |   W  |   E  |   R  |   T  | Meh  |           | Meh  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | CAPSL  |   A  |   S  |D/MNAV|F/KNAV|   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |' / SYMB|
+ * | CAPSL  |A/LAlt|   S  |D/MNAV|F/KNAV|   G  |------|           |------|   H  |   J  |   K  |   L  |;/RAlt|' / SYMB|
  * |--------+------+------+------+------+------| Hyper|           |Hyper |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_EQL,      KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         MO(SYMB),    KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   MEH_T(KC_NO),
-        KC_CAPSLOCK, KC_A,         KC_S,   LT(MNAV,KC_D),   LT(KNAV,KC_F),   KC_G,
+        KC_CAPSLOCK, LALT_T(KC_A), KC_S,   LT(MNAV,KC_D),   LT(KNAV,KC_F),   KC_G,
         KC_LSFT,     CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
         KC_GRV,      KC_QUOT,      LALT(KC_LSFT),  KC_LEFT, META,
                                               ALT_T(KC_APP),  KC_LGUI,
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
              KC_NO,       KC_6,   KC_7,  KC_8,   KC_9,   KC_0,           KC_MINS,
              MEH_T(KC_NO),KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,           KC_BSLS,
-                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          LT(SYMB,KC_QUOT),
+                          KC_H,   KC_J,  KC_K,   KC_L,   RALT_T(KC_SCLN),LT(SYMB,KC_QUOT),
              ALL_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH), KC_RSFT,
                                   META,  KC_DOWN,KC_LBRC,KC_RBRC,        KC_LEAD,
              KC_LALT,        KC_ESC,
