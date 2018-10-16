6b3afcfa429b844425aa02e2e919fcd1c13cb418
Jason Felice <jason.m.felice@gmail.com>
2018-10-16 11:42:48 -0400

    Add tests

--- keyboards/ergodox_ez/keymaps/eraserhd/test.c
+++ keyboards/ergodox_ez/keymaps/eraserhd/test.c
@@ -12,12 +12,68 @@
 
 #include <assert.h>
 
+int offset;
+uint8_t codes[16];
+bool registered[16];
+
 void register_code(uint8_t keycode) {
+    codes[offset] = keycode;
+    registered[offset] = true;
+    offset++;
 }
 
 void unregister_code(uint8_t keycode) {
+    codes[offset] = keycode;
+    registered[offset] = false;
+    offset++;
 }
 
+#define END -1
+#define PRESS -2
+#define RELEASE -3
+
+bool check(const int *test_case) {
+    keyrecord_t record;
+    bool process;
+    int i;
+
+    offset = 0;
+    for (; *test_case != END; test_case += 2) {
+        record.event.pressed = (test_case[0] == PRESS);
+        process = process_record_user(test_case[1], &record);
+        if (process) {
+            if (record.event.pressed)
+                register_code(test_case[1]);
+            else
+                unregister_code(test_case[1]);
+        }
+    }
+
+    ++test_case;
+    for (i = 0; *test_case != END; i++, test_case += 2) {
+        if (i >= offset)
+            return false;
+        if ((test_case[0] == PRESS) != registered[i])
+            return false;
+        if (test_case[1] != codes[i])
+            return false;
+    }
+    if (i != offset)
+        return false;
+
+    return true;
+}
+
+int x_meta_can_be_used_as_x[] = {
+    PRESS, X_META, RELEASE, X_META, END,
+    PRESS, KC_X, RELEASE, KC_X, END
+};
+int x_meta_can_be_used_as_meta[] = {
+    PRESS, X_META, PRESS, KC_Q, RELEASE, KC_Q, RELEASE, X_META, END,
+    PRESS, KC_ESC, RELEASE, KC_ESC, PRESS, KC_Q, RELEASE, KC_Q, END
+};
+
 int main(void) {
-    assert(true);
+    assert(check(x_meta_can_be_used_as_x));
+    assert(check(x_meta_can_be_used_as_meta));
 }
