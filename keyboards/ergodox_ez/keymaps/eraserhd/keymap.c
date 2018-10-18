0777ee3f93ec5d5f69a82ef11b2bb1c980a0bfc2
Jason Felice <jason.m.felice@gmail.com>
2018-10-18 11:26:56 -0400

    remove unused code

--- keyboards/ergodox_ez/keymaps/eraserhd/keymap.c
+++ keyboards/ergodox_ez/keymaps/eraserhd/keymap.c
@@ -136,28 +136,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 };
 
-const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
-};
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-  switch(id) {
-    case 0:
-      if (record->event.pressed) {
-        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-      }
-      break;
-    case 1:
-      if (record->event.pressed) { // For resetting EEPROM
-        eeconfig_init();
-      }
-      break;
-  }
-  return MACRO_NONE;
-};
-
 #include "soft_meta.h"
 
 // Runs just one time when the keyboard initializes.
