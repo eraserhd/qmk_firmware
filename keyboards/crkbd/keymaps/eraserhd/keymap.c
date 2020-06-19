68747feaf588cac81e60cb126af8b0e768a5f93b
Jason Felice <jason.m.felice@gmail.com>
2020-06-19 09:43:09 -0400

    Use Hyper-K for kb->computer

--- keyboards/crkbd/keymaps/eraserhd/keymap.c
+++ keyboards/crkbd/keymaps/eraserhd/keymap.c
@@ -219,14 +219,22 @@ uint32_t layer_state_set_user(uint32_t state)
 {
   if (layer_state_cmp(state, _Window) && !in_window_layer)
   {
-      register_code(KC_F13);
-      unregister_code(KC_F13);
+      register_code(KC_LGUI);
+      register_code(KC_LSHIFT);
+      register_code(KC_LALT);
+      register_code(KC_LCTRL);
+      _delay_ms(15);
+      register_code(KC_K);
+      unregister_code(KC_K);
+      unregister_code(KC_LGUI);
+      unregister_code(KC_LSHIFT);
+      unregister_code(KC_LALT);
+      unregister_code(KC_LCTRL);
       in_window_layer = true;
   }
   if (!layer_state_cmp(state, _Window) && in_window_layer)
   {
-      register_code(KC_F14);
-      unregister_code(KC_F14);
+      tap_code(KC_ESCAPE);
       in_window_layer = false;
   }
   return state;
